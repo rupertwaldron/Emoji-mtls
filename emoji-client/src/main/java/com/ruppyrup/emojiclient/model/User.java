eaf770ae4510333911cb952ac092ee18a0b4e2c3
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-12-07 16:48:52 +0000

    Added note to form

--- emoji-client/src/main/java/com/ruppyrup/emojiclient/model/User.java
+++ emoji-client/src/main/java/com/ruppyrup/emojiclient/model/User.java
@@ -1,22 +1,10 @@
 package com.ruppyrup.emojiclient.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     String name;
     String email;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    String note;
 }
