7594e1eef65cc8ee21f9dcd6aa6ff23cb5426612
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-12-11 19:11:49 +0000

    server and client working

--- emoji-client/src/main/java/com/ruppyrup/emojiclient/EmojiClientApplication.java
+++ emoji-client/src/main/java/com/ruppyrup/emojiclient/EmojiClientApplication.java
@@ -6,20 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmojiClientApplication {
 
-    static {
-        //for localhost testing only
-        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-                new javax.net.ssl.HostnameVerifier(){
-
-                    public boolean verify(String hostname,
-                                          javax.net.ssl.SSLSession sslSession) {
-                        if (hostname.equals("localhost")) {
-                            return true;
-                        }
-                        return false;
-                    }
-                });
-    }
+    // only need if certificate not setup for localhost - First and Last name use localhost
+//    static {
+//        //for localhost testing only
+//        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+//                new javax.net.ssl.HostnameVerifier(){
+//
+//                    public boolean verify(String hostname,
+//                                          javax.net.ssl.SSLSession sslSession) {
+//                        if (hostname.equals("localhost")) {
+//                            return true;
+//                        }
+//                        return false;
+//                    }
+//                });
+//    }
     public static void main(String[] args) {
         SpringApplication.run(EmojiClientApplication.class, args);
     }
