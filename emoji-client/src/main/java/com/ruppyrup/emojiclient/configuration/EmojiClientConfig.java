f942e2f111957083a1f19e6648da94dcecb283f2
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-12-12 13:21:10 +0000

    mutual-ssl working

--- emoji-client/src/main/java/com/ruppyrup/emojiclient/configuration/EmojiClientConfig.java
+++ emoji-client/src/main/java/com/ruppyrup/emojiclient/configuration/EmojiClientConfig.java
@@ -13,33 +13,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class EmojiClientConfig {
 
-//    @Value("${trust.store}")
-//    private Resource trustStore;
-//
-//    @Value("${trust.password}")
-//    private String trustStorePassword;
-
-//    @Bean
-//    @Profile("https")
-//    public RestTemplate httpsRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
-//            CertificateException, MalformedURLException, IOException {
-//
-//
-//        log.info("Creating https resttemplate");
-//        SSLContext sslContext = new SSLContextBuilder()
-//                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
-//                .build();
-//        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-//
-//        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslConFactory).build();
-//        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-//        return new RestTemplate(requestFactory);
-//    }
-
     @Bean
     @Profile("https")
     public RestTemplate httpsRestTemplate(RestTemplateBuilder restTemplateBuilder, SslBundles sslBundles) {
-        return restTemplateBuilder.setSslBundle(sslBundles.getBundle("emoji-client")).build();
+        return restTemplateBuilder.setSslBundle(sslBundles.getBundle("emoji-api")).build();
     }
 
     @Bean
